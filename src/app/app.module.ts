fd10cdb6aa45be455fd6f34ff2a7da187eed34b2
Thomas Nix <tnix2@uncc.edu>
2017-09-09 14:07:57 -0400

    Set up core structure of components and basic styles.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SelectionComponent } from './selection/selection.component';
+import { OutputComponent } from './output/output.component';
+import { CreditsComponent } from './credits/credits.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectionComponent,
+    OutputComponent,
+    CreditsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
